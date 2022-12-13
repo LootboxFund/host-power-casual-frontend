39eab52989db80a46c366099dc0bbf67d7aec394
0xnewton <0xnewton@protonmail.com>
2022-12-13 16:50:48 -0500

    adds gql server

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import { RouterProvider } from "react-router-dom";
+import client from "./api/graphql/client";
 import AuthProvider from "./hooks/useAuth/AuthProvider";
 import routes from "./routes";
 
 const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <RouterProvider router={routes} />;
-    </AuthProvider>
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <RouterProvider router={routes} />;
+      </AuthProvider>
+    </ApolloProvider>
   );
 };
 
