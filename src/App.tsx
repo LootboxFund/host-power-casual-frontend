232f64298af298ac03470e5f55be8686b9c7774f
0xnewton <0xnewton@protonmail.com>
2022-12-13 16:32:51 -0500

    adds firebase auth listener

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./hooks/useAuth/AuthProvider";
 import routes from "./routes";
 
 const App: React.FC = () => {
-  return <RouterProvider router={routes} />;
+  return (
+    <AuthProvider>
+      <RouterProvider router={routes} />;
+    </AuthProvider>
+  );
 };
 
 export default App;
