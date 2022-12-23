2751dabe5577e01b797bc829c1d5c0a1e3d60a96
0xterran <96885027+0xterran@users.noreply.github.com>
2022-12-24 03:46:59 +0700

    google analtyics

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,33 @@ import { RouterProvider } from "react-router-dom";
 import client from "./api/graphql/client";
 import AuthProvider from "./hooks/useAuth/AuthProvider";
 import routes from "./routes";
+import { HeadProvider } from "react-head";
 
 const App: React.FC = () => {
+  const initGTag = () => {
+    (window as any).dataLayer = (window as any).dataLayer || [];
+    function gtag(...args: any[]) {
+      // @ts-ignore
+      (window as any).dataLayer.push(args);
+    }
+    gtag("js", new Date());
+    gtag("config", "G-GVHNC0FVDV");
+  };
+
   return (
-    <ApolloProvider client={client}>
-      <AuthProvider>
-        <RouterProvider router={routes} />
-      </AuthProvider>
-    </ApolloProvider>
+    <HeadProvider>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-GVHNC0FVDV"
+      />
+      {initGTag()}
+      <ApolloProvider client={client}>
+        <AuthProvider>
+          <RouterProvider router={routes} />
+        </AuthProvider>
+      </ApolloProvider>
+    </HeadProvider>
   );
 };
 
